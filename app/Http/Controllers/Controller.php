9b2367e458dc101c07928cbfcbd984413bd6d53e
Caique Bispo <caiquebispo0023@gmail.com>
2024-07-28 16:39:09 -0300

    feat(swagger): finalizando as configurações do swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,30 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="API WITH SWAGGER",
+ *      description="Está é uma api, para fins de aprendizado sobre a utilização do swagger",
+ *      @OA\Contact(
+ *          email="caiquebispodanet86@gmail.com"
+ *      )
+ * )
+ *
+ * @OA\SecurityScheme(
+ *    securityScheme="bearerAuth",
+ *    in="header",
+ *    name="bearerAuth",
+ *    type="http",
+ *    scheme="bearer",
+ *    bearerFormat="JWT",
+ * ),
+ *
+ * @OA\Server(
+ *      url=L5_SWAGGER_CONST_HOST,
+ *      description="API Server"
+ * )
+ */
 abstract class Controller
 {
     //

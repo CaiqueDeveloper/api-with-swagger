9a1d3617780739d695bafd16f408628b3368d201
Caique Bispo <caiquebispo0023@gmail.com>
2024-07-29 16:47:21 -0300

    feat(application): criando metodos no controller

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
-    //
+    public function store()
+    {
+    }
+    public function todos()
+    {
+    }
+    public function todo()
+    {
+    }
+    public function update()
+    {
+    }
+    public function delete()
+    {
+    }
 }

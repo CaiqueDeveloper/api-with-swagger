cf948fb3e827470053590012f5f7a84655b69fd7
Caique Bispo <caiquebispo0023@gmail.com>
2024-07-29 08:24:36 -0300

    refactor(swagger) criando um array de atributos na iniciliazação do swagger no arquivo 'Controller.php'

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,29 +4,28 @@ namespace App\Http\Controllers;
 
 use OpenApi\Attributes as OA;
 
-#[OA\Info(
-    title: "Api With Swagger",
-    version: "1.0.0",
-    description: 'Esta é uma API com objetivo de aprender mais sobre uso do swagger',
-)]
+#[
+    OA\Info(
+        title: "Api With Swagger",
+        version: "1.0.0",
+        description: 'Esta é uma API com objetivo de aprender mais sobre uso do swagger',
+    ),
 
-#[OA\Contact(email: 'caiquebispodanet86@gmail.com')]
+    OA\Contact(email: 'caiquebispodanet86@gmail.com'),
 
-#[OA\SecurityScheme(
-    securityScheme: "bearerAuth",
-    in: "header",
-    name: "bearerAuth",
-    type: "http",
-    scheme: "bearer",
-    bearerFormat: "JWT",
-)]
-#[OA\Server(
-    url: L5_SWAGGER_CONST_HOST,
-    description: "API Server"
-)]
-class OpenApi
-{
-}
+    OA\SecurityScheme(
+        securityScheme: "bearerAuth",
+        in: "header",
+        name: "bearerAuth",
+        type: "http",
+        scheme: "bearer",
+        bearerFormat: "JWT",
+    ),
+    OA\Server(
+        url: L5_SWAGGER_CONST_HOST,
+        description: "API Server"
+    )
+]
 
 abstract class Controller
 {

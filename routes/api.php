eb5dc9478f1dfd09571ab18a1bdcf2d00e31c8a7
Caique Bispo <caiquebispo0023@gmail.com>
2024-07-29 13:22:30 -0300

    fix(UseController): removendo middleware auth no UserController e adicionando nas rotas

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,12 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('/auth/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
     Route::get('/auth/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
 });
+
+Route::controller(UserController::class)->group(function () {
+
+    Route::get('/users', [UserController::class, 'all']);
+    Route::get('/users/{user}', [UserController::class, 'show']);
+    Route::post('/users', [UserController::class, 'create']);
+    Route::put('/users', [UserController::class, 'update']);
+    Route::delete('/users', [UserController::class, 'delete']);
+})->middleware('auth:sanctum');

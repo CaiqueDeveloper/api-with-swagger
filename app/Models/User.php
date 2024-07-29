2dd7a1c064d5fdef709ca5378222f56ae33e8c82
Caique Bispo <caiquebispo0023@gmail.com>
2024-07-29 16:31:47 -0300

    feat(application): criando relacionamento entre user e todo

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -40,5 +41,9 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    public function todos(): BelongsTo
+    {
+        return $this->belongsTo(Todo::class);
     }
 }

0541fccebb7ad30d188e1b723aaa1dfc102f01a3
Caique Bispo <caiquebispo0023@gmail.com>
2024-07-30 14:10:31 -0300

    feat(test): Criando as todo no AuthControllerTest

---
+++ tests/Feature/AuthControllerTest.php
@@ -0,0 +1,12 @@
+<?php
+
+todo('the route register exists');
+todo('verify if is validation error in attempt register user sending empty form data');
+todo('verify if user registered successful');
+
+todo('the route login exists');
+todo('verify if is validation error in attempt login user sending empty form data');
+todo('verify if user authenticate successful');
+
+todo('the route logout exists');
+todo('verify if user logout successful');

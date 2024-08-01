047116c39a7f2bff70799af018e86deb9dc86e36
Caique Bispo <caiquebispo0023@gmail.com>
2024-08-01 20:22:39 -0300

    feat(test): verifique se ao tentar cadastrar uma tarefa com campos vazios está sendo retornado erro de validação

--- tests/Feature/TodoTest.php
+++ tests/Feature/TodoTest.php
@@ -1,7 +1,25 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Laravel\Sanctum\Sanctum;
+
 test('verifique se somente usuários autenticados podem cadastrar uma tarefa', function () {
 
     $this->postJson('/api/todos')
         ->assertUnauthorized();
+});
+test('verifique se ao tentar cadastrar uma tarefa com campos vazios está sendo retornado erro de validação', function () {
+
+    Sanctum::actingAs(User::factory()->create());
+
+    $response = $this->postJson('/api/todos', ['name' => ''])
+        ->assertStatus(422)
+        ->assertJson(
+            fn (AssertableJson $json) =>
+            $json->where('success', false)
+                ->where('message', 'Validation errors')
+                ->where('data.name.0', 'The name field is required.')
+                ->etc()
+        );
 });

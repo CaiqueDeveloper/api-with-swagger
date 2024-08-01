bf713dbed1b6e8becc0c42224f90864f969065ae
Caique Bispo <caiquebispo0023@gmail.com>
2024-07-31 21:44:46 -0300

    feat(teste): verify if is validation error in attempt register user sending empty form data

--- tests/Feature/AuthControllerTest.php
+++ tests/Feature/AuthControllerTest.php
@@ -1,11 +1,25 @@
 <?php
 
+use Illuminate\Testing\Fluent\AssertableJson;
+
 it('the route register exists and return status code 422', function () {
 
     $response = $this->postJson('/api/auth/register', [])
         ->assertStatus(422);
 });
-todo('verify if is validation error in attempt register user sending empty form data');
+it('verify if is validation error in attempt register user sending empty form data', function () {
+
+    $this->postJson('/api/auth/register', [])
+        ->assertJson(
+            fn (AssertableJson $json) =>
+            $json->where('success', false)
+                ->where('message', 'Validation errors')
+                ->where('data.name.0', 'The name field is required.')
+                ->where('data.email.0', 'The email field is required.')
+                ->where('data.password.0', 'The password field is required.')
+                ->etc()
+        );
+});
 todo('verify if user registered successful');
 
 todo('the route login exists');

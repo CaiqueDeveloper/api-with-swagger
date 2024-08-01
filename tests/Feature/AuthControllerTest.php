2a9321f85eff222968acf6dada84c5fb7116e416
Caique Bispo <caiquebispo0023@gmail.com>
2024-07-31 21:35:26 -0300

    feat(teste): the route register exists and return status code 422

--- tests/Feature/AuthControllerTest.php
+++ tests/Feature/AuthControllerTest.php
@@ -1,6 +1,10 @@
 <?php
 
-todo('the route register exists');
+it('the route register exists and return status code 422', function () {
+
+    $response = $this->postJson('/api/auth/register', [])
+        ->assertStatus(422);
+});
 todo('verify if is validation error in attempt register user sending empty form data');
 todo('verify if user registered successful');
 

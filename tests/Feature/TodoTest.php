be2360245b673dd31111d33cff770383ade7393d
Caique Bispo <caiquebispo0023@gmail.com>
2024-08-01 19:28:54 -0300

    feat(test) criado todos para o teste da funcionalidade Todos

--- tests/Feature/TodoTest.php
+++ tests/Feature/TodoTest.php
@@ -1,7 +1,17 @@
 <?php
 
-test('example', function () {
-    $response = $this->get('/');
+todo('verifique se somente usuários autenticados podem cadastrar uma tarefa');
+todo('verifique se ao tentar cadastrar uma tarefa com campos vazios está sendo retornado erro de validação');
+todo('verifique se uma tarefa foi criada com sucesso e associada ao usuário logado');
 
-    $response->assertStatus(200);
-});
+todo('verifique se somente usuários autenticados podem ver todas as tarefas cadastradas');
+todo('verifique se está sendo retornado às tarefas criadas');
+todo('verifique se uma tarefa foi criada com sucesso e associada ao usuário logado');
+
+todo('verifique se somente usuários autenticados podem editar uma tarefa');
+todo('verifique se ao tentar editar uma tarefa com campos vazios está sendo retornado erros de validação');
+todo('verifique se uma tarefa foi editada com sucesso');
+
+todo('verifique se somente usuários autenticados podem deletar uma tarefa');
+todo('verifique se, ao tentar deletar uma tarefa, se informar o id erro de validação está sendo retornado');
+todo('verifique se uma tarefa foi deletada com sucesso');

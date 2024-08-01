f228e804b5be0246ab0e24538314dfaebb68c715
Caique Bispo <caiquebispo0023@gmail.com>
2024-08-01 20:12:55 -0300

    feat(test): verifique se somente usuários autenticados podem cadastrar uma tarefa

--- tests/Feature/TodoTest.php
+++ tests/Feature/TodoTest.php
@@ -1,17 +1,7 @@
 <?php
 
-todo('verifique se somente usuários autenticados podem cadastrar uma tarefa');
-todo('verifique se ao tentar cadastrar uma tarefa com campos vazios está sendo retornado erro de validação');
-todo('verifique se uma tarefa foi criada com sucesso e associada ao usuário logado');
+test('verifique se somente usuários autenticados podem cadastrar uma tarefa', function () {
 
-todo('verifique se somente usuários autenticados podem ver todas as tarefas cadastradas');
-todo('verifique se está sendo retornado às tarefas criadas');
-todo('verifique se uma tarefa foi criada com sucesso e associada ao usuário logado');
-
-todo('verifique se somente usuários autenticados podem editar uma tarefa');
-todo('verifique se ao tentar editar uma tarefa com campos vazios está sendo retornado erros de validação');
-todo('verifique se uma tarefa foi editada com sucesso');
-
-todo('verifique se somente usuários autenticados podem deletar uma tarefa');
-todo('verifique se, ao tentar deletar uma tarefa, se informar o id erro de validação está sendo retornado');
-todo('verifique se uma tarefa foi deletada com sucesso');
+    $this->postJson('/api/todos')
+        ->assertUnauthorized();
+});

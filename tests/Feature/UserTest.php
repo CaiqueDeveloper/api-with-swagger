4669bd50d2358c850b7f2da74c1df4cb85f1f294
Caique Bispo <caiquebispo0023@gmail.com>
2024-08-05 19:13:13 -0300

    feat(test) verique se ao tentar atulizar os dados de usuário não logao um erro de Unauthorized é retornado

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -1,6 +1,10 @@
 <?php
 
-todo('verique se ao tentar atulizar os dados de usuário não estando logado um error de validação é retornado');
+it('verique se ao tentar atulizar os dados de usuário não logao um erro de Unauthorized é retornado', function () {
+
+    $this->putJson('/api/user', [])
+        ->assertUnauthorized();
+});
 todo('verifique se o usuário auteticado consguiu atulizar as infromações dele com sucesso');
 todo('verique se ao tentar deletar os dados de usuário não estando logado um error de validação é retornado');
 todo('verifique se o usuário  foi deletado com seucesso');

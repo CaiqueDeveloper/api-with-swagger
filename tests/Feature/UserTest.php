a8b428b5cabb279eed2e9ba350fd0379361cbf3c
Caique Bispo <caiquebispo0023@gmail.com>
2024-08-05 19:09:46 -0300

    feat(test) criando todos de case teste user

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -1,7 +1,6 @@
 <?php
 
-test('example', function () {
-    $response = $this->get('/');
-
-    $response->assertStatus(200);
-});
+todo('verique se ao tentar atulizar os dados de usuário não estando logado um error de validação é retornado');
+todo('verifique se o usuário auteticado consguiu atulizar as infromações dele com sucesso');
+todo('verique se ao tentar deletar os dados de usuário não estando logado um error de validação é retornado');
+todo('verifique se o usuário  foi deletado com seucesso');
